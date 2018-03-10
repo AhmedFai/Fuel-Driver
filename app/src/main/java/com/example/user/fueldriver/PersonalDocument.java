abd0a6893219d595ac48f8bac9c45a3dba90a0da
Mukul <mukulraw199517@gmail.com>
2018-03-10 18:57:54 +0530

    Fuel Driver App

--- app/src/main/java/com/example/user/fueldriver/PersonalDocument.java
+++ app/src/main/java/com/example/user/fueldriver/PersonalDocument.java
@@ -1,16 +1,33 @@
 package com.example.user.fueldriver;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.io.IOException;
 
 public class PersonalDocument extends AppCompatActivity {
 
     Toolbar toolbar;
-
+    EditText address, validation;
+    ImageView add, val;
+    TextView upload, validate;
+    Button submit;
+    private static final int GALLERY_REQUEST = 1;
+    private static final int GALLERY_REQUEST2 = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +36,14 @@ public class PersonalDocument extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        upload = (TextView)findViewById(R.id.image);
+        validate = (TextView)findViewById(R.id.uploadValidation);
+        add = (ImageView)findViewById(R.id.adressImage);
+        val = (ImageView)findViewById(R.id.validationImage);
+        submit = (Button)findViewById(R.id.submit);
+
         setSupportActionBar(toolbar);
+
 
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
@@ -34,5 +58,59 @@ public class PersonalDocument extends AppCompatActivity {
             }
         });
 
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+        validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY_REQUEST2);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+
+                add.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }else if (requestCode == GALLERY_REQUEST2 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+
+                val.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/user/fueldriver/VehicleDocuments.java
+++ app/src/main/java/com/example/user/fueldriver/VehicleDocuments.java
@@ -1,14 +1,26 @@
 package com.example.user.fueldriver;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VehicleDocuments extends AppCompatActivity {
 
     Toolbar toolbar;
+    List<String> company;
+    Spinner spine;
+    TextView rc, insu;
+    private static final int GALLERY_REQUEST = 1;
+    private static final int GALLERY_REQUEST2 = 2;
 
 
     @Override
@@ -17,6 +29,20 @@ public class VehicleDocuments extends AppCompatActivity {
         setContentView(R.layout.activity_vehicle_documents);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        spine = (Spinner)findViewById(R.id.spinner);
+        rc = (TextView)findViewById(R.id.uploadRC);
+        insu = (TextView)findViewById(R.id.uploadInsu);
+        company = new ArrayList<>();
+
+        company.add("Insurance Company");
+        company.add("Chaloo Company");
+        company.add("Badiya Company");
+        company.add("Jhakkas Company");
+
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, company);
+        spine.setAdapter(dataAdapter);
 
         setSupportActionBar(toolbar);
 
@@ -30,6 +56,27 @@ public class VehicleDocuments extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+
+        rc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+        insu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY_REQUEST2);
             }
         });
 
